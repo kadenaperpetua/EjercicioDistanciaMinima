edb855305cb3002dfaa43cc757da87c7118f7169
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-09 12:20:08 -0500

    ELI: Corrige el lazo for, para que se ejecute desde el segundo hasta el penultimo elemento

--- src/com/eli/distancia/Main.java
+++ src/com/eli/distancia/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
 
         int[] prueba= {4,8,6,1,2,9,4};
+
         obtenerIndiceDeLaDistanciaMinima(prueba);
 
 
@@ -16,20 +17,28 @@ public class Main {
         if(entrada.length>=2){
             int indiceMenorDistancia= 0;
             int menorDistancia= Math.abs(entrada[0]-entrada[1]);
+            System.out.println("entrada = " + entrada.length);
 
-            for(int i=1;i<entrada.length-2;i++){
+            for(int i=1;i<entrada.length-1;i++){
 
                 int distanciaIterada= Math.abs(entrada[i]-entrada[i+1]);
+                System.out.println("i = " + i);
+                System.out.println("distanciaIterada = " + distanciaIterada);
+
 
                 if (distanciaIterada<menorDistancia){
                     indiceMenorDistancia=i;
                     menorDistancia = distanciaIterada;
                 }
             }
-        }
-        System.out.println(indiceMenorDistancia);
-        return indiceMenorDistancia;
 
+            System.out.println(indiceMenorDistancia);
+            return indiceMenorDistancia;
+
+        }else{
+            System.out.println("El arreglo debe contener mas de dos elementos");
+            return 0;
+        }
 
     }
 }
